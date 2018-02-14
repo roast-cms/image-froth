daa82892d3244d7f78358c74e94902d7fa85b5c5
Dmitri <d@eurekaking.com>
2018-02-14 17:45:53 +0700

    Manage constants

--- examples/index.js
+++ examples/index.js
@@ -1,21 +1,29 @@
 import { froth, getFroth } from "../src"
 
-const IMAGE_FROTH_SERVER =
-  "https://res.cloudinary.com/analog-cafe/image/upload/"
-const IMAGE_FROTH_OPTIONS = "c_scale,fl_progressive"
-const IMAGE_FROTH_SIZES = {
-  i: "40",
-  t: "280",
-  s: "520",
-  m: "1268",
-  l: "1800"
+const FROTH_CONSTANTS = {
+  server: "https://res.cloudinary.com/analog-cafe/image/upload/",
+  transformations: "c_scale,fl_progressive",
+  sizes: {
+    i: "40",
+    t: "280",
+    s: "520",
+    m: "1268",
+    l: "1800"
+  },
+  placeholder:
+    "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
 }
-const dot =
-  "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
 
-console.log( "froth object from src string",
-  froth({ src: "image-froth_1681956_9ad677d272a84ebc9360ad6199372f8b" })
+console.log(
+  "froth object from src string",
+  froth(
+    { src: "image-froth_1681956_9ad677d272a84ebc9360ad6199372f8b" },
+    FROTH_CONSTANTS
+  )
 )
-console.log( "froth src string from URL",
-  getFroth("https://res.cloudinary.com/analog-cafe/image/upload/c_scale,fl_progressive,w_1268/image-froth_1681956_9ad677d272a84ebc9360ad6199372f8b.jpg")
+console.log(
+  "froth src string from URL",
+  getFroth(
+    "https://res.cloudinary.com/analog-cafe/image/upload/c_scale,fl_progressive,w_1268/image-froth_1681956_9ad677d272a84ebc9360ad6199372f8b.jpg"
+  )
 )
--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
-const IMAGE_FROTH_SERVER =
-  "https://res.cloudinary.com/analog-cafe/image/upload/"
-const IMAGE_FROTH_OPTIONS = "c_scale,fl_progressive"
-const IMAGE_FROTH_SIZES = {
-  i: "40",
-  t: "280",
-  s: "520",
-  m: "1268",
-  l: "1800"
-}
-const dot =
-  "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-
-
 
   // example froth src:
 // image-froth_1681956_9ad677d272a84ebc9360ad6199372f8b
-export const froth = (options = {}) => {
-  let src = options.src || dot
+export const froth = (options = {}, CONSTANTS) => {
+  let src = options.src || CONSTANTS.placeholder
   let size = options.size || "m"
   let type = options.type || "jpg"
   let crop = options.crop || "none"
 
-  let width = IMAGE_FROTH_SIZES[size]
+  let width = CONSTANTS.sizes[size]
   let height = null
   let ratio = 0
 
@@ -33,14 +19,14 @@ export const froth = (options = {}) => {
 
   if (src.includes("image-froth") && !src.includes("/")) {
     if (crop === "none") {
-      src = `${IMAGE_FROTH_SERVER}${IMAGE_FROTH_OPTIONS},w_${width}/${src}.${type}`
+      src = `${CONSTANTS.server}${CONSTANTS.transformations},w_${width}/${src}.${type}`
       ratio = src.split("image-froth_").pop().split("_").shift() / 1000000
       height = Math.round(width / ratio, 1)
     } else if (crop === "square") {
       ratio = 1
       height = width
       src =
-      `${IMAGE_FROTH_SERVER}c_fill,g_auto,w_${width},h_${height}/${src}.${type}`
+      `${CONSTANTS.server}c_fill,g_auto,w_${width},h_${height}/${src}.${type}`
     }
   }
   return {
@@ -52,7 +38,7 @@ export const froth = (options = {}) => {
   }
 }
 
-export const getFroth = src => {
+export const getFroth = (src) => {
   if (src.includes("data:image")) return "default"
   let id
   id = src.split("\\").pop().split("/").pop() // get rid of domain and pathname
